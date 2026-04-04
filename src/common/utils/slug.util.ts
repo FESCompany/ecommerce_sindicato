c68b198fb473f44e7d4b94a124f7fc47ae5a0146
Felipe Lino <felipegadelha2004@gmail.com>
2026-04-03 21:53:39 -0300

    fix: removed and SlugService created

--- src/common/utils/slug.util.ts
+++
@@ -1,8 +0,0 @@
-export function generateSlug(value: string): string {
-  return value
-    .toLowerCase()
-    .trim()
-    .replace(/\s+/g, '-')
-    .replace(/[^\w-]+/g, '')
-    .replace(/--+/g, '-');
-}
