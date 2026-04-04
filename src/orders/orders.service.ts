cdf00b0f78a56d37dbc0e6bc2d5073cefb7b6601
Felipe Lino <felipegadelha2004@gmail.com>
2026-04-04 17:45:58 -0300

    fix: removing configService
    fixing the error handling

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -10,7 +10,6 @@ import { Prisma } from 'src/generated/prisma/client';
 import { CreatePaymentProviderOrderDto } from './dtos/create-payment-provider-order.dto';
 import { PaymentProviderService } from 'src/payment-provider/payment-provider.service';
 import { BillingService } from 'src/billing/billing.service';
-import { ConfigService } from '@nestjs/config';
 
 type ItemData = {
   productId: string;
@@ -24,17 +23,16 @@ export class OrdersService {
     private prismaService: PrismaService,
     private paymentProviderService: PaymentProviderService,
     private billingService: BillingService,
-    private configService: ConfigService,
   ) {}
   async order(userId: string, id: string) {
+    const buyer = await this.prismaService.user.findUnique({
+      where: { id: userId },
+      select: { id: true },
+    });
+
+    if (!buyer) throw new NotFoundException('User not found');
+
     try {
-      const buyer = await this.prismaService.user.findUnique({
-        where: { id: userId },
-        select: { id: true },
-      });
-
-      if (!buyer) throw new NotFoundException('User not found');
-
       return this.prismaService.order.findFirst({
         where: {
           id,
@@ -60,14 +58,14 @@ export class OrdersService {
     userId: string;
     orderBy?: Prisma.OrderOrderByWithRelationInput;
   }) {
+    const buyer = await this.prismaService.user.findUnique({
+      where: { id: params.userId },
+      select: { id: true },
+    });
+
+    if (!buyer) throw new NotFoundException('User not found');
+
     try {
-      const buyer = await this.prismaService.user.findUnique({
-        where: { id: params.userId },
-        select: { id: true },
-      });
-
-      if (!buyer) return null;
-
       return this.prismaService.order.findMany({
         skip: params.skip,
         take: params.take,
@@ -102,48 +100,48 @@ export class OrdersService {
   async createPaymentProviderOrder(
     createPaymentProviderOrderDto: CreatePaymentProviderOrderDto,
   ) {
-    try {
-      const buyer = await this.prismaService.user.findUnique({
-        where: { id: createPaymentProviderOrderDto.userId },
-      });
+    const buyer = await this.prismaService.user.findUnique({
+      where: { id: createPaymentProviderOrderDto.userId },
+    });
 
-      if (!buyer) throw new NotFoundException('User not found');
+    if (!buyer) throw new NotFoundException('User not found');
 
-      // search all products at once (perfomance)
-      const products = await this.prismaService.product.findMany({
-        where: {
-          id: {
-            in: createPaymentProviderOrderDto.items.map((i) => i.productId),
-          },
+    // search all products at once (perfomance)
+    const products = await this.prismaService.product.findMany({
+      where: {
+        id: {
+          in: createPaymentProviderOrderDto.items.map((i) => i.productId),
         },
+      },
+    });
+
+    if (products.length !== createPaymentProviderOrderDto.items.length)
+      throw new NotFoundException('One or more products not found');
+
+    let total = 0;
+    const itemsData: ItemData[] = [];
+
+    for (const item of createPaymentProviderOrderDto.items) {
+      const product = products.find((p) => p.id === item.productId);
+      if (!product) throw new NotFoundException('Product not found');
+      if (product.userId === createPaymentProviderOrderDto.userId)
+        throw new BadRequestException('You cannot buy your own product');
+      if (product.stock < item.quantity)
+        throw new BadRequestException(
+          `Insufficient stock for product ${product.name}`,
+        );
+      total += product.price * item.quantity;
+      itemsData.push({
+        productId: product.id,
+        price: product.price,
+        quantity: item.quantity,
       });
+    }
 
-      if (products.length !== createPaymentProviderOrderDto.items.length)
-        throw new NotFoundException('One or more products not found');
+    // seller id
+    const sellerId = products[0].userId;
 
-      let total = 0;
-      const itemsData: ItemData[] = [];
-
-      for (const item of createPaymentProviderOrderDto.items) {
-        const product = products.find((p) => p.id === item.productId);
-        if (!product) throw new NotFoundException('Product not found');
-        if (product.userId === createPaymentProviderOrderDto.userId)
-          throw new BadRequestException('You cannot buy your own product');
-        if (product.stock < item.quantity)
-          throw new BadRequestException(
-            `Insufficient stock for product ${product.name}`,
-          );
-        total += product.price * item.quantity;
-        itemsData.push({
-          productId: product.id,
-          price: product.price,
-          quantity: item.quantity,
-        });
-      }
-
-      // seller id
-      const sellerId = products[0].userId;
-
+    try {
       // create order + items
       const order = await this.prismaService.order.create({
         data: {
