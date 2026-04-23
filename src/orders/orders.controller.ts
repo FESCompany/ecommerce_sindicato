b47ba16d6d0a79e4abecf7299c4cb4d7b2720c2e
Felipe Lino <felipegadelha2004@gmail.com>
2026-04-23 16:44:35 -0300

    fix: add productsService
    feat: add sellerOrders and sellerOrder method

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -12,22 +12,57 @@ import {
 import { OrdersService } from './orders.service';
 import { AuthGuard } from 'src/auth/auth.guard';
 import { CreateOrderDto } from './dtos/create-order.dto';
+import { SellerGuard } from 'src/auth/seller.guard';
+import { ProductsService } from 'src/products/product.service';
 
 @Controller('orders')
 export class OrdersController {
-  constructor(private ordersService: OrdersService) {}
+  constructor(
+    private ordersService: OrdersService,
+    private productsService: ProductsService,
+  ) {}
   @Get('')
   @UseGuards(AuthGuard)
   async orders(@Request() req: Request) {
     const user = req['user'] as { sub: string };
     return await this.ordersService.orders({ userId: user.sub });
   }
+
+  @Get('seller-orders')
+  @UseGuards(AuthGuard, SellerGuard)
+  async sellerOrders(@Request() req: Request) {
+    const seller = req['user'] as { sub: string };
+    return await this.ordersService.sellerOrders({ userId: seller.sub });
+  }
+
+  @Get('seller-orders/:id')
+  @UseGuards(AuthGuard, SellerGuard)
+  async sellerOrder(@Request() req: Request, @Param('id') id: string) {
+    const seller = req['user'] as { sub: string };
+    const order = await this.ordersService.sellerOrder(seller.sub, id);
+    const orderItems = await Promise.all(
+      order!.items.map(async (item) => {
+        const { url } = await this.productsService.getProductImageUrl(
+          item.productId,
+        );
+        return {
+          ...item,
+          product: {
+            image: url,
+          },
+        };
+      }),
+    );
+    return { ...order, items: orderItems };
+  }
+
   @Get(':id')
   @UseGuards(AuthGuard)
   async order(@Request() req: Request, @Param('id') id: string) {
     const user = req['user'] as { sub: string };
     return await this.ordersService.order(user.sub, id);
   }
+
   @Post('')
   @UseGuards(AuthGuard)
   async create(
