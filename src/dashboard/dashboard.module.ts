22dc1f70815451214137d6517d563b24dd03e825
Felipe Lino <felipegadelha2004@gmail.com>
2026-04-24 15:40:09 -0300

    fix: adding required providers and imports

--- src/dashboard/dashboard.module.ts
+++ src/dashboard/dashboard.module.ts
@@ -1,21 +1,20 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
-import { CryptoService } from 'src/crypto/crypto.service';
-import { PaymentProviderService } from 'src/payment-provider/payment-provider.service';
-import { UsersService } from 'src/users/user.service';
-import { PaymentGatewayService } from 'src/payment-gateway/payment-gateway.service';
-import { ProductsService } from 'src/products/product.service';
 import { DashboardService } from './dashboard.service';
 import { DashboardController } from './dashboard.controller';
+import { UsersService } from 'src/users/user.service';
+import { PaymentProviderService } from 'src/payment-provider/payment-provider.service';
+import { CryptoService } from 'src/crypto/crypto.service';
+import { PaymentGatewayService } from 'src/payment-gateway/payment-gateway.service';
+import { PaymentGatewayModule } from 'src/payment-gateway/payment-gateway.module';
 
 @Module({
-  imports: [],
+  imports: [PaymentGatewayModule],
   providers: [
     DashboardService,
-    ProductsService,
     PrismaService,
-    PaymentProviderService,
     UsersService,
+    PaymentProviderService,
     CryptoService,
     PaymentGatewayService,
   ],
