0aefdc3f346e0f022440f3479679a20ed945976c
Felipe Lino <felipegadelha2004@gmail.com>
2026-05-11 22:25:47 -0300

    fix: removing unnecessary guard

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -9,7 +9,6 @@ import {
 import { UsersService } from './user.service';
 import { AuthGuard } from 'src/auth/auth.guard';
 import { SellerGuard } from 'src/auth/seller.guard';
-import { PaymentProviderGuard } from 'src/payment-provider/payment-provider.guard';
 import { SubscriptionGuard } from 'src/subscription/subscription.guard';
 
 @Controller('users')
@@ -17,7 +16,7 @@ export class UsersController {
   constructor(private userService: UsersService) {}
 
   @HttpCode(HttpStatus.OK)
-  @UseGuards(AuthGuard, SellerGuard, SubscriptionGuard, PaymentProviderGuard)
+  @UseGuards(AuthGuard, SellerGuard, SubscriptionGuard)
   @Get('clients')
   async clients(@Request() req: Request) {
     const user = req['user'] as {
