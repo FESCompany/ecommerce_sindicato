02e064a0f19f673694c104868201979289d6870d
Felipe Lino <felipegadelha2004@gmail.com>
2026-05-11 22:23:36 -0300

    fix: add PaymentGatewayModule and WebhookController

--- src/subscription/subscription.module.ts
+++ src/subscription/subscription.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { SubscriptionService } from './subscription.service';
 import { SubscriptionController } from './subscription.controller';
 import { PrismaService } from 'src/prisma.service';
+import { WebhookController } from './webhook.controller';
+import { PaymentGatewayModule } from 'src/payment-gateway/payment-gateway.module';
 
 @Module({
-  imports: [],
+  imports: [PaymentGatewayModule],
   providers: [SubscriptionService, PrismaService],
-  controllers: [SubscriptionController],
+  controllers: [SubscriptionController, WebhookController],
   exports: [SubscriptionService],
 })
 export class SubscriptionModule {}
